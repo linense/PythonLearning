ac12209cd4a4b1a6730dad37d8434c024b628493
Gregorio Loriguillo Gonzalez <georg_loriguillo@yahoo.de>
2024-04-18 06:57:00 +0200

    Break-Dateien ergänzt

---
+++ .history/Loops/Break_20240417213449.py
@@ -0,0 +1,10 @@
+for i in range(1,11):
+    if(i==5):
+        break
+    print(i)
+
+number = input("Please enter your number: ")
+for i in range(1,11):
+    if(int(number)*i==60):
+        break
+    print(int(number)*i)
---
+++ .history/Loops/Break_20240417221030.py
@@ -0,0 +1,17 @@
+for i in range(1,11):
+    if(i==5):
+        break
+    print(i)
+
+number = input("Please enter your number: ")
+for i in range(1,11):
+    if(int(number)*i==60):
+        break
+    print(int(number)*i)
+
+number = input("Please enter your number: ")
+for i in range(1,11):
+    if(int(number)*i%10==0):
+        continue
+    print(int(number)*i)
+    
