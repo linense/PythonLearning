469b8c66cbf34e7feb046a544f8ed15d753adb33
linense <80359175+linense@users.noreply.github.com>
2024-08-02 07:29:46 +0200

    Delete Strings directory

--- Strings/StringHandling2.py
+++
@@ -1,5 +0,0 @@
-name=input("Please enter your first and last name: ")
-address=input("Please enter your address: ")
-profile=input("Please enter your job profile: ")
-
-print("Person " + name + " lives in " + address + " working as a " + profile) 
