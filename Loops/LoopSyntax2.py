cc9201eda4ea6c0126876f42b9ccc4e586a06d0d
linense <80359175+linense@users.noreply.github.com>
2024-08-02 07:29:35 +0200

    Delete Loops directory

--- Loops/LoopSyntax2.py
+++
@@ -1,7 +0,0 @@
-# For loops with starting and final range
-
-z=7
-
-for i in range(1,11):
-    print(str(z) + " * " + str(i) + " = " + str(z*i))
-    
--- Loops/Loopsyntax3.py
+++
@@ -1,7 +0,0 @@
-# For loop with starting and final range
-
-for i in range(1,11,2):
-    print(i)
-for i in range(10,0,-1):
-    print(i)
-    
