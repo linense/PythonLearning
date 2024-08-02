c020060b438c1c82e333a1b5e058f9dbf675c784
linense <80359175+linense@users.noreply.github.com>
2024-08-02 07:28:35 +0200

    Delete ConditionHandling directory

--- ConditionHandling/SimpleOneCondition.py
+++
@@ -1,9 +0,0 @@
-# Take input from user - Number
-# Check if number is even or odd
-# Print if it is even, do nothing if it is odd
-data = input("Please enter your number: ")
-data = int(data)
-
-if(data%2==0):
-    print(str(data) + " is an even number")
-    
