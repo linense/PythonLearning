fcbf3491d058c7e20c79e85c4050e4aaddf95cca
Gregorio Loriguillo Gonzalez <georg_loriguillo@yahoo.de>
2024-09-17 06:57:21 +0200

    Kapitel 17

--- 17_Advanced_Python/AutomationStart.py
+++ 17_Advanced_Python/AutomationStart.py
@@ -1,5 +1,10 @@
-# Whenever we are importing any module, that module executable code will run
+# Whenever we are importing any module, that module executable code will run automatically
 import PyModule
 
 PyModule.testingPythonModuleFunction()
 PyModule.sum(100,200)
+
+# To use class functions, we need to create an object of the class
+
+obj=PyModule.A()
+obj.testingPythonClassFunction()
