3db53a7300c2801b089342ccde61ac6be35d9cd4
Gregorio Loriguillo Gonzalez <georg_loriguillo@yahoo.de>
2024-09-23 18:38:00 +0200

    Weiterarbeit Kapitel 18

--- 18_ExcelData/18_01_ReadExcel.py
+++ 18_ExcelData/18_01_ReadExcel.py
@@ -4,10 +4,17 @@ import openpyxl
 # Load Workbook
 wk = openpyxl.load_workbook("C:\\Users\\a315707\\OneDrive - Deutsche Telekom AG\\Schulungen\\Programmierung\\Python\\PythonLearning\\18_ExcelData\\TestData.xlsx")
 
-print(wk.sheetnames) # List sheets
-print(wk.active.title) # Active Sheet
+#print(wk.sheetnames) # List sheets
+#print(wk.active.title) # Active Sheet title
 
 # Create object of sheet on which you want to work
 
-sh = wk['Tabelle1']
-print(sh.title)
+sh = wk['Tabelle1'] # Move to Workbook level
+#print(sh.title) # Active Sheet title
+print(sh['A3'].value)
+
+c1=sh.cell(1,1) # Move to cell level - first row, first column
+print(c1.value)
+
+c2=sh.cell(column=3, row=2) # without these arguments, it's first row then column
+print(c2.value)
