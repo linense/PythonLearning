a5261a75697ecb5ee67f5c74545d81a78dccb60b
Gregorio Loriguillo Gonzalez <georg_loriguillo@yahoo.de>
2024-09-25 21:13:54 +0200

    Fertigstellung Kapitel 18

--- 18_ExcelData/18_02_WriteExcel.py
+++ 18_ExcelData/18_02_WriteExcel.py
@@ -1,6 +1,18 @@
 import openpyxl
 import openpyxl.workbook
 
-wk = openpyxl.workbook()
+wk = openpyxl.Workbook()
+sh = wk.active
+sh.title="HelloTestingWorld"
+sh['A4'].value="www.theTestingWorld.com"
+wk.create_sheet(title="TestingW")
+sh1=wk['TestingW']
+sh1['A3']="+918743913121"
 
-print(wk.active.title)
+# Remove Sheet
+
+#wk.remove_sheet(wk['HelloTestingWorld'])
+wk.remove(wk['HelloTestingWorld'])
+
+wk.save("C:\\Users\\a315707\\OneDrive - Deutsche Telekom AG\\Schulungen\\Programmierung\\Python\\PythonLearning\\18_ExcelData\\18_02_OutputExcel.xlsx")
+
