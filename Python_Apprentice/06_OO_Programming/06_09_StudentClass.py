8a3afb35124e20b0378382f7ae5951f77d65bc3b
Gregorio Loriguillo Gonzalez <georg_loriguillo@yahoo.de>
2025-02-05 17:11:25 +0100

    Python Kapitel 6

--- Python_Apprentice/06_OO_Programming/06_09_StudentClass.py
+++ Python_Apprentice/06_OO_Programming/06_09_StudentClass.py
@@ -27,6 +27,35 @@ class Student:
         print("Student: ", self.__name)
         print(self.__gpa, self.__clubs, self.__active)
 
+def get_students(student_details_list):
+    students_list = []
+    for student_details in student_details_list:
+        if 'name' not in student_details or 'gpa' not in student_details:
+            continue
+        s = Student(student_details['name'], student_details['gpa'])
+
+        if 'clubs' in student_details:
+            for club in student_details['clubs']:
+                s.add_club(club)
+
+        if 'active' in student_details:
+            s.set_active(student_details['active'])
+        
+        students_list.append(s)
+    return students_list
+
 s = Student('James', 3.8)
 s.add_club("Yoga")
 s.print_details()
+
+student_details_list = [
+    {'name': 'Nina', 'gpa': 3.6, 'clubs': ['tennis', 'chess']},
+    {'name': 'Emily', 'gpa': 3.9, 'clubs': ['tennis'], 'active': False},
+    {'name': 'Michael', 'gpa': 3.2, 'clubs': ['football', 'chess']},
+    {'name': 'Joe', 'gpa': 3.9, 'is_honors_student': True}
+]
+
+students = get_students(student_details_list)
+#print(students)
+for student in students:
+    student.print_details()
